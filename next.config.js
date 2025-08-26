485ddcab64eadc57597f0595aad56e934ca4cd15
Dwayne Clark <clark.dwayne@gmail.com>
2025-08-26 02:46:23 -0400

    commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   // Standard Next.js configuration for Netlify deployment
   reactStrictMode: true,
   swcMinify: true,
+  
+  // Disable ESLint during build to prevent hanging
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
